d0a2fda03282d7a683f821e0694771127a22be06
Antonio Sanchez <antonio.buap@gmail.com>
2021-04-14 08:30:40 -0500

    feat: Tune PID parameters

--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,7 @@ int main() {
   /**
    * TODO: Initialize the pid variable.
    */
-  pid.Init(1.0, 0, 0);
+  pid.Init(0.1, 1e-9, 7.5);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
                      uWS::OpCode opCode) {
